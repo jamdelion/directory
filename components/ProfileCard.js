48bde67315617e0f5618193f79681a6800c5a117
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2022-03-08 00:24:37 +0000

    Initial commit plus profilecard

---
+++ components/ProfileCard.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+    width: 80%;
+    height: 100%;
+    border-radius: 40%;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    display: flex;
+    justify-content: center;
+`;
+
+export default function ProfileCard({}) {
+
+    return (
+        <StyledContainer>
+            {/* Profile pic */}
+            
+            {/* Name */}
+            {/* Cohort */}
+
+            {/* Short description */}
+
+            {/* Timeline */}
+
+            {/* Contact details button */}
+
+            {/* Social icons */}
+            <p>Hi!</p>
+        </StyledContainer>
+    )
+
+}
