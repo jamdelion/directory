802e4c87ecc25619d3695949117eedc3bdbb5608
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2022-03-08 00:31:47 +0000

    Add placeholders for future content

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -4,29 +4,42 @@ import styled from "styled-components";
 const StyledContainer = styled.div`
     width: 80%;
     height: 100%;
-    border-radius: 40%;
+    border-radius: 10%;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     display: flex;
     justify-content: center;
+    flex-direction: column;
+    align-items: center;
 `;
+
+const ProfilePic = styled.div`
+    border: solid 1px black;
+    border-radius: 100%;
+    width: 200px;
+    height: 200px;
+    margin: 2rem;
+`
 
 export default function ProfileCard({}) {
 
     return (
         <StyledContainer>
             {/* Profile pic */}
+            <ProfilePic/>
             
             {/* Name */}
+            <p>Fake Name</p>
             {/* Cohort */}
+            <p>FAC 21</p>
 
             {/* Short description */}
+            <p>Lorem ipsum grinder skate gnarly kickflip heel toeside</p>
 
             {/* Timeline */}
 
             {/* Contact details button */}
 
             {/* Social icons */}
-            <p>Hi!</p>
         </StyledContainer>
     )
 
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          FAC Directory
         </h1>
 
         <p className={styles.description}>
@@ -27,12 +27,10 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
@@ -40,7 +38,6 @@ export default function Home() {
             className={styles.card}
           >
             <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
@@ -49,7 +46,6 @@ export default function Home() {
           >
             <h2>Deploy &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
         </div>
