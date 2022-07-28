c49b44bbcd3e0ce7117da62b98922adab018b897
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2022-07-28 17:21:39 +0100

    Expand facesline and protect index with auth

--- components/FacesLine.js
+++ components/FacesLine.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Face from "./Face";
 
@@ -18,21 +18,28 @@ const StyledLineContainer = styled.div`
 `;
 
 export default function FacesLine({ people, title }) {
-  // console.log("PEOPLE", people)
+  const [isExpanded, setIsExpanded] = useState(false);
 
   // needs to randomly show first few faces and be expandable to show the others
+
+  const faces = people.map((x) => (
+    <Face name={x.name} key={x.name} github={x.github} />
+  ));
 
   return (
     <StyledLineContainer>
       <div style={{ alignSelf: "flex-start" }}>
         <p>{title}</p>
       </div>
-      <button style={{ alignSelf: "flex-end" }}>Expand</button>
+      <button
+        onClick={() => setIsExpanded(!isExpanded)}
+        style={{ alignSelf: "flex-end" }}
+      >
+        {isExpanded ? "Minimise -" : "Expand +"}
+      </button>
 
       <StyledFacesContainer>
-        {people.map((x) => (
-          <Face name={x.name} key={x.name} github={x.github} />
-        ))}
+        {isExpanded ? faces : faces.slice(0, 4)}
       </StyledFacesContainer>
     </StyledLineContainer>
   );
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { supabase } from "../database/supabaseClient";
 import FacesLine from "../components/FacesLine";
 import Nav from "../components/Nav";
+import { useUser } from "@auth0/nextjs-auth0";
 
 export async function getStaticProps() {
   const peopleData = await supabase.from("people").select("*");
@@ -13,12 +14,13 @@ export async function getStaticProps() {
   return {
     props: {
       people: peopleData.data,
-      cohorts: cohortData.data
+      cohorts: cohortData.data,
     },
   };
 }
 
 export default function Home({ people, cohorts }) {
+  const { user, error, isLoading } = useUser();
   return (
     <div className={styles.container}>
       <Head>
@@ -27,45 +29,58 @@ export default function Home({ people, cohorts }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Nav/>
+      <Nav />
 
       <main className={styles.main}>
         <h1 className={styles.title}>FAC Directory</h1>
 
-        <Link href="/add">
-          <a style={{margin: "2rem"}}>+ Add FAC member</a>
-        </Link>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error.message}</div>
+        ) : (
+          <>
+            {user && (
+              <>
+                <Link href="/add">
+                  <a style={{ margin: "2rem" }}>+ Add FAC member</a>
+                </Link>
 
-        <p className={styles.description}>
-          <input placeholder="FAC22" />
-          <code className={styles.code}>Search for a cohort/name...</code>
-        </p>
+                <p className={styles.description}>
+                  <input placeholder="FAC22" />
+                  <code className={styles.code}>
+                    Search for a cohort/name...
+                  </code>
+                </p>
 
+                {/* for a few (or all??) cohorts, show a sample of faces, i.e. one line each */}
 
-        {/* for a few (or all??) cohorts, show a sample of faces, i.e. one line each */}
+                {cohorts.map((cohort) => {
+                  return (
+                    <FacesLine
+                      key={cohort.id}
+                      title={cohort.name}
+                      people={people.filter((p) => p.cohort === cohort.name)}
+                    />
+                  );
+                })}
 
-        {cohorts.map(cohort => {
-          return (
-            <FacesLine key={cohort.id} title={cohort.name} people={people.filter(p => p.cohort === cohort.name)}/>
-          )
-        })}
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-          </a>
-
-        </div>
-        <Link href="/people/jamdelion">
-          <a>Go to Jo profile</a>
-        </Link>
-        <Link href="/cohorts/fac21">
-          <a>Go to FAC21 profile</a>
-        </Link>
+                {/* <div className={styles.grid}> */}
+                  <Link href="/people/jamdelion">
+                    <a className={styles.card}>
+                      <h2>Go to Jo profile &rarr;</h2>
+                    </a>
+                  </Link>
+                  <Link href="/cohorts/fac21">
+                    <a className={styles.card}>
+                      <h2>Go to FAC21 profile &rarr;</h2>
+                    </a>
+                  </Link>
+                {/* </div> */}
+              </>
+            )}
+          </>
+        )}
       </main>
 
       <footer className={styles.footer}>
